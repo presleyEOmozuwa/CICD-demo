941a716bb5ec5b09f3054d73b18d34f916e6cd9d
Presley Omozuwa <presleyomozuwa90@gmail.com>
2024-03-13 23:32:28 -0500

    changes made

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/api/users', (req, res) => {
-    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}]
+    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: Beckky, lastname: Cole}]
     res.send({"users": users});
 });
 
 router.get('/api/userbyid', (req, res) => {
-    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}]
+    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: Beckky, lastname: Cole}]
     res.send({"user": users[2]});
 });
 
