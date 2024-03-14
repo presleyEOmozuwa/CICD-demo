37ee1f2fdccd57bb38b18ddfcfb945164d36a785
Presley Omozuwa <presleyomozuwa90@gmail.com>
2024-03-14 01:14:38 -0500

    modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/api/users', (req, res) => {
-    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: Beckky, lastname: Cole}]
+    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: "Beckky", lastname: "Cole"}, {id: 5, firstname: "James", lastname: "Barnes"}]
     res.send({"users": users});
 });
 
 router.get('/api/userbyid', (req, res) => {
-    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: Beckky, lastname: Cole}]
+    const users = [ {id: 1, firstname: "Smith", lastname: "Blake"}, {id: 2, firstname: "James", lastname: "Eddie"}, {id: 3, firstname: "Mary", lastname: "Payne"}, {id: 4, firstname: "Beckky", lastname: "Cole"}, {firstname: "James", lastname: "Barnes"}]
     res.send({"user": users[2]});
 });
 
