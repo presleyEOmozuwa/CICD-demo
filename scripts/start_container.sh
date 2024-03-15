0b77c4b9a4c400ac29e46828c3abbec6a5c4c6aa
Presley Omozuwa <presleyomozuwa90@gmail.com>
2024-03-15 00:53:37 -0500

    added files

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+
+
+#!/bin/sh
+set -e
+
+# Pull Docke image from docker hub
+docker pull rubblestech3400/cicd-demo
+
+# Run the the image to create container
+docker run -it -d -p 5001:5000 --name node_server rubblestech3400/cicd-demo
